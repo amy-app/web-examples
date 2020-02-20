30c87fc99701c72b08b4425b9c82d4fc0e6a47a1
Jürgen Brandstetter <jurgen@jaipuna.com>
2020-02-20 11:16:51 +0100

    initial examples

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # web-examples
+
 Example implementation of the AMY UI by using amy.app API
+
+## Get user token
+
+So a user can interact with Amy the user needs a user Token. You can generate a user token via our API at https://learn.amy.app/__/api/ . Under point `Auth` you will find `generate-user-token`.
+You will need three parameters: `oauth_consumer_key`, `api_key` and `user_id`.
+
+The return value will be a JSON looking like this
+
+```
+{
+    token: "long token string",
+    amy_user_id: "Jaipuna_juhu_user1",
+}
+```
+
+## Start webserver
+
+To start the example project all you need to do is run a local webserver. eg. `npx serve`. This should server your local project on port `5000`. No you can open the example page via http://localhost:5000
---
+++ examples/amy-helpers.js
@@ -0,0 +1,93 @@
+// ----------------- FIREBASE CONFIG ---------------
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ20dFy6oeNt3uF_URIBD5lllZs-l0b6o",
+    projectId: "amy-ac",
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig, "AMY");
+const assignmentCollection = app.firestore().collection("StudentAssignments");
+const bubbleCollection = app.firestore().collection("ExerciseBubbles");
+
+/**
+ * Authenticates a user via a firebase token
+ * and returns the user object.
+ * You get the token via the following api
+ * https://learn-dev1-amy-app.firebaseapp.com/__/api/#/Auth/get_auth_v1_0_generate_user_token
+ *
+ * @param token firebase token string
+ * @return firebase user object
+ */
+async function authViaToken(token) {
+    return app.auth().signInWithCustomToken(token);
+}
+
+/**
+ * Creates an assignment based on assignment options
+ * @param assignmentOptions assignment config object
+ * @param onChange onChange(assignmentSnap, bubblesSnap) function that is called when the assignemtn or bubble changes
+ * @return callback that stops the firebase realtime listners
+ */
+function createAssignment(assignmentOptions, onChange) {
+    // Assignemnt and Bubble snaps for current assignment
+    let assignmentSnap;
+    let bubbleSnaps;
+
+    // generate random assignmentId
+    const assignmentReference = assignmentCollection.doc();
+    const assignmentId = assignmentReference.id;
+
+    // create assignment
+    assignmentReference.set(assignmentOptions);
+
+    // listen to assignment changes
+    const stopAssignment = assignmentReference.onSnapshot(_assignmentSnap => {
+        assignmentSnap = _assignmentSnap;
+        onChange(assignmentSnap, bubbleSnaps);
+    });
+
+    // listent to bubble changes for the assignment
+    const stopBubbles = bubbleCollection
+        .orderBy("createdAt", "asc")
+        .where("studentAssignmentId", "==", assignmentId)
+        .where("studentId", "==", assignmentOptions.studentId)
+        .onSnapshot(_bubbleSnaps => {
+            bubbleSnaps = _bubbleSnaps;
+            onChange(assignmentSnap, bubbleSnaps);
+        });
+
+    return () => {
+        stopAssignment();
+        stopBubbles();
+    };
+}
+
+/**
+ * Creates an assignment based on assignment options
+ * @param assignmentOptions assignment config object
+ * @param onChange onChange(assignmentSnap, bubblesSnap) function that is called when the assignemtn or bubble changes
+ * @return callback that stops the firebase realtime listners
+ */
+
+/**
+ * Creates an assignment based on assignment options and token
+ *
+ *
+ * @param {*} assignmentOptions assignment config object
+ * @param {*} token firebase token
+ * @param {*} onChange onChange(assignmentSnap, bubblesSnap) function that is called when the assignemtn or bubble changes
+ * @return Promise<stopEvent> Calling the stopEvent() will stop all listeners
+ */
+async function startAssignment(assignmentOptions, token, onChange) {
+    return authViaToken(token).then(student => {
+        const studentId = student.user.uid;
+        return createAssignment({ ...assignmentOptions, studentId }, onChange);
+    });
+}
+
+function clickOption(selectedPath, bubbleId) {
+    // set the path/option so AMY can update the data
+    // and tell you if your selection was in/correct
+    bubbleCollection.doc(bubbleId).update({
+        selectedPath,
+    });
+}
